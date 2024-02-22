3aead30a60a76f0a229e8f5e9b04f970e59212bc
AnatolSalau <salovanatoly5@mail.ru>
2024-02-22 10:14:58 +0300

    add line Transaction Chart

---
+++ src/main/kotlin/samples/charts/linearTransactionsChart.kt
@@ -0,0 +1,97 @@
+package samples.charts
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+
+/**
+ * Represents a group of Transactions
+ * @param maxTransaction the max transaction value in the list of transactions previously calculated in some repository.
+ * @param transactions list of transactions per second.
+ */
+data class TransactionsPerSecond(
+    val maxTransaction: Double,
+    val transactions: List<TransactionRate>
+)
+
+/**
+ * Represents a transaction rate.
+ * @param timeStamp the time stamp of the transaction.
+ * @param transactionsPerSecondValue the quantity of transactions made per second.
+ */
+data class TransactionRate(
+    val timeStamp: Long,
+    val transactionsPerSecondValue: Double
+)
+
+@Composable
+fun drawLinearTransactionsChart(
+    modifier: Modifier = Modifier,
+    transactionsPerSecond: TransactionsPerSecond
+) {
+    if (transactionsPerSecond.transactions.isEmpty()) return
+
+    Canvas(modifier = modifier) {
+        // Total number of transactions.
+        val totalRecords = transactionsPerSecond.transactions.size
+
+        // Maximum distance between dots (transactions)
+        val lineDistance = size.width / (totalRecords + 1)
+
+        // Canvas height
+        val cHeight = size.height
+
+        // Add some kind of a "Padding" for the initial point where the line starts.
+        var currentLineDistance = 0F + lineDistance
+
+        transactionsPerSecond.transactions.forEachIndexed { index, transactionRate ->
+            if (totalRecords >= index + 2) {
+                drawLine(
+                    start = Offset(
+                        x = currentLineDistance,
+                        y = calculateYCoordinate(
+                            higherTransactionRateValue = transactionsPerSecond.maxTransaction,
+                            currentTransactionRate = transactionRate.transactionsPerSecondValue,
+                            canvasHeight = cHeight
+                        )
+                    ),
+                    end = Offset(
+                        x = currentLineDistance + lineDistance,
+                        y = calculateYCoordinate(
+                            higherTransactionRateValue = transactionsPerSecond.maxTransaction,
+                            currentTransactionRate = transactionsPerSecond.transactions[index + 1].transactionsPerSecondValue,
+                            canvasHeight = cHeight
+                        )
+                    ),
+                    color = Color(40, 193, 218),
+                    strokeWidth = Stroke.DefaultMiter
+                )
+            }
+            currentLineDistance = currentLineDistance + lineDistance
+        }
+    }
+}
+
+/**
+ * Calculates the Y pixel coordinate for a given transaction rate.
+ *
+ * @param higherTransactionRateValue the highest rate value in the whole list of transactions.
+ * @param currentTransactionRate the current transaction RATE while iterating the list of transactions.
+ * @param canvasHeight the canvas HEIGHT for draw the linear chart.
+ *
+ * @return [Float] Y coordinate for a transaction rate.
+ */
+private fun calculateYCoordinate(
+    higherTransactionRateValue: Double,
+    currentTransactionRate: Double,
+    canvasHeight: Float
+): Float {
+    val maxAndCurrentValueDifference = (higherTransactionRateValue - currentTransactionRate)
+        .toFloat()
+    val relativePercentageOfScreen = (canvasHeight / higherTransactionRateValue)
+        .toFloat()
+    return maxAndCurrentValueDifference * relativePercentageOfScreen
+}
