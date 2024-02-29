22f2e23a55765b3f5b07ccf589b2582ac46a66ae
AnatolSalau <salovanatoly5@mail.ru>
2024-02-29 15:51:05 +0300

    Drag rectangular by mouse

--- src/main/kotlin/samples/components/drawRectangularByMouse.kt
+++ src/main/kotlin/samples/components/drawRectangularByMouse.kt
@@ -2,15 +2,20 @@ package samples.components
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
@@ -20,14 +25,18 @@ import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
-const val MIN_ZOOM_SIZE: Float = 50f
+const val MIN_ZOOM_SIZE: Float = 20f
 
 @Composable
 @OptIn(ExperimentalFoundationApi::class)
@@ -52,7 +61,9 @@ fun drawRectangularByDragMouse() {
     var zoomYLeft by remember { mutableStateOf(0f) }
     var zoomWidth by remember { mutableStateOf(0f) }
     var zoomHeight by remember { mutableStateOf(0f) }
-    var zoomColorULong: ULong by remember { mutableStateOf(Color.Transparent.value) }
+    var zoomColorULong: ULong by remember { mutableStateOf(Color.Green.value) }
+
+    var zoomButtonText by remember { mutableStateOf("Увеличить") }
 
     LaunchedEffect(interactionSource) {
         interactionSource.interactions.collect { interaction ->
@@ -75,150 +86,211 @@ fun drawRectangularByDragMouse() {
             }
         }
     }
+    MaterialTheme {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .pointerInput(Unit) {
+                detectTapGestures { offset: Offset ->
+                    // Touch coordinates on image
+                    xTap = offset.x
+                    yTap = offset.y;
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Gray)
-        .pointerInput(Unit) {
-            detectTapGestures { offset: Offset ->
-                // Touch coordinates on image
-                xTap = offset.x
-                yTap = offset.y;
+                    zoomXLeft = 0f
+                    zoomYLeft = 0f
+                    zoomWidth = 0f
+                    zoomHeight = 0f
 
-                zoomXLeft = 0f
-                zoomYLeft = 0f
-                zoomWidth = 0f
-                zoomHeight = 0f
-                zoomColorULong = Color.Transparent.value
+                    xDragStart = 0f
+                    yDragStart = 0f
+                    xDragEnd = 0f
+                    yDragEnd = 0f
+
+                    zoomButtonText = "Увеличить"
+
+                }
             }
-        }
-        .pointerInput(Unit) {
-            var interaction: DragInteraction.Start? = null
+            .pointerInput(Unit) {
+                var interaction: DragInteraction.Start? = null
 
-            detectDragGestures(
-                onDragStart = { offset ->
-                    run {
-                        xTap = offset.x
-                        yTap = offset.y
+                detectDragGestures(
+                    onDragStart = { offset ->
+                        run {
+                            xTap = offset.x
+                            yTap = offset.y
 
-                        zoomXLeft = offset.x
-                        zoomYLeft = offset.y
-                        zoomColorULong = Color.Transparent.value
+                            xDragStart = offset.x
+                            yDragStart = offset.x
+                            xDragEnd = 0f
+                            yDragEnd = 0f
 
-                    }
-                    coroutineScope.launch {
-                        interaction = DragInteraction.Start()
-                        interaction?.run {
-                            interactionSource.emit(this)
+                            zoomButtonText = "Увеличить"
+
                         }
+                        coroutineScope.launch {
+                            interaction = DragInteraction.Start()
+                            interaction?.run {
+                                interactionSource.emit(this)
+                            }
 
-                    }
-                },
-                onDrag = { change: PointerInputChange, dragAmount: Offset ->
-                    run {
-                        xTap += dragAmount.x
-                        yTap += dragAmount.y
+                        }
+                    },
+                    onDrag = { change: PointerInputChange, dragAmount: Offset ->
+                        run {
+                            xTap += dragAmount.x
+                            yTap += dragAmount.y
+                            zoomXLeft = xTap
+                            zoomYLeft = yTap
 
-                        fun calculateCoordinates(
-                            dragCoordinate: Float,
-                            tapCoordinate: Float,
-                            minValue: Float
-                        ): Float {
-                            val size = -(dragCoordinate - tapCoordinate)
+                            fun calculateCoordinates(
+                                dragCoordinate: Float,
+                                tapCoordinate: Float
+                            ): Float {
+                                val size = dragCoordinate - tapCoordinate
 
-                            if (size.absoluteValue != dragCoordinate && size.absoluteValue != tapCoordinate && size.absoluteValue > minValue) {
+                                if (abs(dragCoordinate.absoluteValue - tapCoordinate.absoluteValue) <= MIN_ZOOM_SIZE) return 0f
+
+
                                 return size
+                            }
+
+                            var width = calculateCoordinates(xDragStart, xTap)
+                            var height = calculateCoordinates(yDragStart, yTap)
+
+                            if (width.absoluteValue < MIN_ZOOM_SIZE || height.absoluteValue < MIN_ZOOM_SIZE) {
+                                zoomWidth = 0f
+                                zoomHeight = 0f
                             } else {
-                                return 0f
+                                zoomWidth = width
+                                zoomHeight = height
+                            }
+
+                        }
+                    },
+                    onDragCancel = {
+
+                        xDragEnd = xTap
+                        yDragEnd = xTap
+
+                        coroutineScope.launch {
+                            interaction?.run {
+                                interactionSource.emit(DragInteraction.Cancel(this))
+                            }
+                        }
+                    },
+                    onDragEnd = {
+                        xDragEnd = xTap
+                        yDragEnd = xTap
+
+                        coroutineScope.launch {
+                            interaction?.run {
+                                interactionSource.emit(DragInteraction.Stop(this))
                             }
                         }
 
-                        zoomWidth = calculateCoordinates(xDragStart, xTap, MIN_ZOOM_SIZE)
-                        zoomHeight = calculateCoordinates(yDragStart, yTap, MIN_ZOOM_SIZE)
-
-                        if ((zoomWidth.absoluteValue > MIN_ZOOM_SIZE) && (zoomHeight.absoluteValue > MIN_ZOOM_SIZE)) {
-                            zoomColorULong = Color.Green.value
-                        } else {
-                            zoomColorULong = Color.Transparent.value
-                        }
-
                     }
-                },
-                onDragCancel = {
-                    zoomWidth = 0f
-                    zoomHeight = 0f
-                    zoomColorULong = Color.Transparent.value
-                    coroutineScope.launch {
-                        interaction?.run {
-                            interactionSource.emit(DragInteraction.Cancel(this))
-                        }
-                    }
-                },
-                onDragEnd = {
-                    coroutineScope.launch {
-                        interaction?.run {
-                            interactionSource.emit(DragInteraction.Stop(this))
-                        }
-                    }
-
-                }
-            )
-        }
-    ) {
-        Canvas(
-            modifier = Modifier
+                )
+            }
         ) {
-            drawRoundRect(
-                color = Color(zoomColorULong),
-                size = Size(
-                    width = zoomWidth,
-                    height = zoomHeight
-                ),
-                topLeft = Offset(
-                    zoomXLeft,
-                    zoomYLeft
-                ),
-                cornerRadius = CornerRadius(10f),
-                style = Stroke(
-                    width = 4f,
-                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 20f), 5f),
-                ),
-            )
+            Canvas(
+                modifier = Modifier.zIndex(1f)
+            ) {
+                drawRoundRect(
+                    color = Color(zoomColorULong),
+                    size = Size(
+                        width = zoomWidth,
+                        height = zoomHeight
+                    ),
+                    topLeft = Offset(
+                        zoomXLeft,
+                        zoomYLeft
+                    ),
+                    cornerRadius = CornerRadius(10f),
+                    style = Stroke(
+                        width = 4f,
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 20f), 5f),
+                    ),
+                )
+            }
+
+            Canvas(
+                modifier = Modifier
+                    .offset { IntOffset(xTap.roundToInt(), yTap.roundToInt()) }
+            ) {
+                drawCircle(
+                    color = Color.Blue,
+                    radius = 5f
+                )
+            }
+
+            Canvas(
+                modifier = Modifier
+                    .offset { IntOffset(xDragStart.roundToInt(), yDragStart.roundToInt()) }
+            ) {
+                drawCircle(
+                    color = Color.Red,
+                    radius = 5f
+                )
+            }
+            if (xDragEnd != 0f && yDragStart != 0f && dragType == "stop"
+                && zoomWidth.absoluteValue > MIN_ZOOM_SIZE
+                && zoomHeight.absoluteValue > MIN_ZOOM_SIZE
+            ) {
+                Button(
+                    onClick = {
+                        zoomButtonText = "Увеличен"
+                    },
+                    modifier = Modifier
+                        .offset { IntOffset(xDragEnd.roundToInt(), yDragEnd.roundToInt()) }
+                ) {
+                    Text(zoomButtonText)
+                }
+            }
+            Row(modifier = Modifier.fillMaxSize()) {
+                Column {
+                    Spacer(Modifier.height(10.dp))
+                    Text(tapCoordinates)
+                    Text(xTap.toString())
+                    Text(yTap.toString())
+
+                    Spacer(Modifier.height(10.dp))
+                    Text(xyDragStartCoordinates)
+                    Text(xDragStart.toString())
+                    Text(yDragStart.toString())
+
+                    Spacer(Modifier.height(10.dp))
+                    Text(dragAmountCoordinates)
+                    Text(xDragEnd.toString())
+                    Text(yDragEnd.toString())
+
+                    Spacer(Modifier.height(10.dp))
+                    Text("Drag status : $dragType")
+
+                    Spacer(Modifier.height(10.dp))
+                    Text("Zoom width : ${zoomWidth.absoluteValue}")
+                    Text("Zoom height : ${zoomHeight.absoluteValue}")
+                    Text("Zoom color : ${if (zoomColorULong == Color.Transparent.value) "Transparent" else "Green"}")
+                    Spacer(Modifier.height(10.dp))
+                }
+                Column(modifier = Modifier
+                    .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "The is wll be picture")
+                    Image(
+                        painter = painterResource("erythrocytes.png"),
+                        contentDescription = "image",
+                        contentScale = ContentScale.Fit,
+                        alignment = Alignment.CenterStart,
+                        modifier = Modifier.height(40.dp).padding(5.dp)
+                    )
+                }
+            }
         }
-
-        Box(
-            Modifier
-                .offset { IntOffset(xTap.roundToInt(), yTap.roundToInt()) }
-                .background(Color.Blue)
-                .size(10.dp)
-        )
-
-        Spacer(Modifier.height(10.dp))
-        Text(tapCoordinates)
-        Text(xTap.toString())
-        Text(yTap.toString())
-
-        Spacer(Modifier.height(10.dp))
-        Text(xyDragStartCoordinates)
-        Text(xDragStart.toString())
-        Text(yDragStart.toString())
-
-        Spacer(Modifier.height(10.dp))
-        Text(dragAmountCoordinates)
-        Text(xDragEnd.toString())
-        Text(yDragEnd.toString())
-
-        Spacer(Modifier.height(10.dp))
-        Text("Drag status : $dragType")
-
-        Spacer(Modifier.height(10.dp))
-        Text("Zoom width : ${zoomWidth.absoluteValue}")
-        Text("Zoom height : ${zoomHeight.absoluteValue}")
-        Text("Zoom color : ${if (zoomColorULong == Color.Transparent.value) "Transparent" else "Green"}")
-
-        Spacer(Modifier.height(10.dp))
 
     }
+
 }
 
 class TempZoom(val zoomWidth: Float, val zoomHeight: Float, val zoomColorULong: ULong);
